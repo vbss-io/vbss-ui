010d670de627918a0366cf4de8606476b0bb1b90
Vitor Bastos <vbss-services@outlook.com>
2024-06-11 19:36:36 -0300

    feat: install and config storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: async (config) => {
+    config.plugins?.push(
+      tsConfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
+      })
+    )
+    return config;
+  },
+};
+export default config;
