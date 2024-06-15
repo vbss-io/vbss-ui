73f106372c3681052a6dcb245e8d223d0b587fa8
Vitor Bastos <vbss-services@outlook.com>
2024-06-15 14:29:41 -0300

    feat: add Chip component storybook

---
+++ src/components/Chip/index.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Chip } from ".";
+
+const meta: Meta<typeof Chip> = {
+  title: "Components/Chip",
+  component: Chip,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Chip>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const argTypes: any = {
+  variant: {
+    options: ["primary", "secondary", "outline"],
+    control: { type: "select" },
+  },
+  size: {
+    options: ["sm", "md", "lg"],
+    control: { type: "select" },
+  },
+  rounded: {
+    options: ["sm", "md", "lg", "full"],
+    control: { type: "select" },
+  },
+  font: {
+    options: ["regular", "bold", "extrabold"],
+    control: { type: "select" },
+  },
+};
+
+export const Primary: Story = {
+  args: {
+    children: "Content",
+    variant: "primary",
+  },
+  argTypes,
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "Content",
+    variant: "secondary",
+  },
+  argTypes,
+};
+
+export const Outline: Story = {
+  args: {
+    children: "Content",
+    variant: "outline",
+  },
+  argTypes,
+};
