7475fde9ba6f7ddbf2d130f88a67d181e5aafe33
Vitor Bastos <vbss-services@outlook.com>
2024-06-15 14:41:59 -0300

    chore: Update tailwind CSS variables for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,30 +19,12 @@ export const theme = {
   },
   extend: {
     colors: {
-      border: "hsl(var(--border))",
-      input: "hsl(var(--input))",
-      ring: "hsl(var(--ring))",
       background: "hsl(var(--background))",
-      foreground: "hsl(var(--foreground))",
       primary: {
         DEFAULT: "hsl(var(--primary))",
-        foreground: "hsl(var(--primary-foreground))",
       },
       secondary: {
         DEFAULT: "hsl(var(--secondary))",
-        foreground: "hsl(var(--secondary-foreground))",
-      },
-      destructive: {
-        DEFAULT: "hsl(var(--destructive))",
-        foreground: "hsl(var(--destructive-foreground))",
-      },
-      muted: {
-        DEFAULT: "hsl(var(--muted))",
-        foreground: "hsl(var(--muted-foreground))",
-      },
-      accent: {
-        DEFAULT: "hsl(var(--accent))",
-        foreground: "hsl(var(--accent-foreground))",
       },
       popover: {
         DEFAULT: "hsl(var(--popover))",
@@ -52,11 +34,6 @@ export const theme = {
         DEFAULT: "hsl(var(--card))",
         foreground: "hsl(var(--card-foreground))",
       },
-    },
-    borderRadius: {
-      lg: "var(--radius)",
-      md: "calc(var(--radius) - 2px)",
-      sm: "calc(var(--radius) - 4px)",
     },
     keyframes: {
       "accordion-down": {
