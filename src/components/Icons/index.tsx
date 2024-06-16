2b991007424f5d20fc99fa1aa02f291fc009b7ac
Vitor Bastos <vbss-services@outlook.com>
2024-06-16 14:16:43 -0300

    feat: Add new icons to Icons component

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,8 +1,12 @@
 import {
+  Eye,
+  EyeSlash,
   GithubLogo,
   Hamburger,
   LinkedinLogo,
+  Minus,
   Moon,
+  Plus,
   Sun,
   X,
 } from "@phosphor-icons/react";
@@ -15,7 +19,11 @@ export type Icons =
   | "hamburger"
   | "x"
   | "sun"
-  | "moon";
+  | "moon"
+  | "plus"
+  | "minus"
+  | "eye"
+  | "eye-slash";
 
 export type IconsMap = {
   [key in Icons]: React.ReactNode;
@@ -31,4 +39,8 @@ export const IconsMap: Record<Icons, React.ReactNode> = {
   x: <X className={cn} />,
   sun: <Sun className={cn} />,
   moon: <Moon className={cn} />,
+  plus: <Plus className={cn} />,
+  minus: <Minus className={cn} />,
+  eye: <Eye className={cn} />,
+  "eye-slash": <EyeSlash className={cn} />,
 };
