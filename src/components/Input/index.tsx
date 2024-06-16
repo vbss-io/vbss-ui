290339ab66983f5a56948602ec756aaf0204e379
Vitor Bastos <vbss-services@outlook.com>
2024-06-16 17:15:25 -0300

    refactor: Update Input component to accept a buttonComponent

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,18 @@
+import { Button, ButtonProps } from "@/components/Button";
 import { IconsMap } from "@/components/Icons";
 import { cn } from "@/lib/utils";
 import { VariantProps, cva } from "class-variance-authority";
 import { ComponentProps, forwardRef, useState } from "react";
 
 const defaultInput =
-  "flex w-full border px-3 py-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none";
+  "flex w-full border px-3 py-2 focus-visible:outline-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none";
 
 const inputStyles = cva("", {
   variants: {
     variant: {
-      primary: `${defaultInput} focus-visible:ring-primary`,
-      secondary: `${defaultInput} focus-visible:ring-secondary`,
-      outline: `${defaultInput} border-primary focus-visible:ring-1 focus-visible:ring-primary focus-visible:ring-offset-0`,
+      primary: `${defaultInput} focus-visible:border-primary`,
+      secondary: `${defaultInput} focus-visible:border-secondary`,
+      outline: `${defaultInput} border-primary focus-visible:border-secondary`,
       ghost: `${defaultInput} focus-visible:ring-0`,
     },
     height: {
@@ -54,6 +55,7 @@ type InputProps = ComponentProps<"input"> &
     type?: "text" | "password" | "number";
     label?: string;
     showPasswordSwitch?: boolean;
+    buttonProps?: ButtonProps;
   };
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
@@ -70,6 +72,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       placeholder,
       label,
       showPasswordSwitch,
+      buttonProps,
       ...props
     },
     ref
@@ -84,7 +87,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     };
 
     return (
-      <div className="flex relative w-full flex-col gap-1">
+      <div className="flex w-full flex-col gap-1">
         {label && (
           <label
             htmlFor={inputId}
@@ -102,49 +105,63 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
             {label}
           </label>
         )}
-        <input
-          id={inputId}
-          type={inputType}
-          disabled={disabled}
-          placeholder={placeholder}
-          ref={ref}
-          value={props.value}
-          className={cn(
-            inputStyles({
-              variant,
-              height,
-              rounded,
-              font,
-              fontSize,
-              className,
-            })
+        <div className="flex flex-row item-end">
+          <div className="flex relative w-full">
+            <input
+              id={inputId}
+              type={inputType}
+              disabled={disabled}
+              placeholder={placeholder}
+              ref={ref}
+              value={props.value}
+              className={cn(
+                `${buttonProps && "!rounded-e-none"}`,
+                inputStyles({
+                  variant,
+                  height,
+                  rounded,
+                  font,
+                  fontSize,
+                  className,
+                })
+              )}
+              {...props}
+            />
+            {showPasswordSwitch && !showPassword && (
+              <div
+                className={`${disabled ? "pointer-events-none" : "cursor-pointer"} select-none absolute w-auto right-0 bottom-0`}
+                onClick={switchPasswordVisibility}
+              >
+                <div
+                  className={`relative opacity-50 bottom-[0.3rem] right-1.5 ${height === "sm" ? "w-5" : height === "md" ? "w-6" : "w-7"}`}
+                >
+                  {IconsMap["eye"]}
+                </div>
+              </div>
+            )}
+            {showPasswordSwitch && showPassword && (
+              <div
+                className={`${disabled ? "pointer-events-none" : "cursor-pointer"} select-none absolute w-auto right-0 bottom-0`}
+                onClick={switchPasswordVisibility}
+              >
+                <div
+                  className={`relative opacity-50 bottom-[0.3rem] right-1.5 ${height === "sm" ? "w-5" : height === "md" ? "w-6" : "w-7"}`}
+                >
+                  {IconsMap["eye-slash"]}
+                </div>
+              </div>
+            )}
+          </div>
+          {buttonProps && (
+            <Button
+              className={`rounded-s-none`}
+              rounded={rounded}
+              disabled={disabled}
+              size={height}
+              {...buttonProps}
+            />
           )}
-          {...props}
-        />
-        {showPasswordSwitch && !showPassword && (
-          <div
-            className={`${disabled ? "pointer-events-none" : "cursor-pointer"} select-none absolute w-auto self-end bottom-0`}
-            onClick={switchPasswordVisibility}
-          >
-            <div
-              className={`relative opacity-50 bottom-[0.3rem] right-1.5 ${height === "sm" ? "w-5" : height === "md" ? "w-6" : "w-7"}`}
-            >
-              {IconsMap["eye"]}
-            </div>
-          </div>
-        )}
-        {showPasswordSwitch && showPassword && (
-          <div
-            className={`cursor-pointer select-none absolute w-auto self-end bottom-0`}
-            onClick={switchPasswordVisibility}
-          >
-            <div
-              className={`relative opacity-50 bottom-[0.3rem] right-1.5 ${height === "sm" ? "w-5" : height === "md" ? "w-6" : "w-7"}`}
-            >
-              {IconsMap["eye-slash"]}
-            </div>
-          </div>
-        )}
+        </div>
       </div>
     );
   }
