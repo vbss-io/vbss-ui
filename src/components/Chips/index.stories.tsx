8e125d0cc81390ad3a82ccffebffb02828e20444
Vitor Bastos <vbss-services@outlook.com>
2024-06-17 07:58:40 -0300

    feat: Add Chips component storybook

---
+++ src/components/Chips/index.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Chips } from ".";
+
+const meta: Meta<typeof Chips> = {
+  title: "Components/Chips",
+  component: Chips,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Chips>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const argTypes: any = {
+  gap: {
+    options: ["sm", "md", "lg"],
+    control: { type: "select" },
+  },
+  chipsProps: {
+    control: { type: "object" },
+  },
+};
+
+export const Primary: Story = {
+  args: {
+    chips: ["Content", "Content", "Content"],
+    gap: "md",
+    chipsProps: {
+      variant: "primary",
+    },
+  },
+  argTypes,
+};
+
+export const Secondary: Story = {
+  args: {
+    chips: ["Content", "Content", "Content"],
+    chipsProps: {
+      variant: "secondary",
+    },
+  },
+  argTypes,
+};
+
+export const Outline: Story = {
+  args: {
+    chips: ["Content", "Content", "Content"],
+    chipsProps: {
+      variant: "outline",
+    },
+  },
+  argTypes,
+};
