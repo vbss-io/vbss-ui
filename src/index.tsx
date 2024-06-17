7ff031a0086d5dba794cbf9907a58135d0cee971
Vitor Bastos <vbss-services@outlook.com>
2024-06-17 08:34:01 -0300

    feat: Add Chips and HistoryNav components

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import "./index.css";
 /* eslint-disable react-refresh/only-export-components */
 export * from "./components/Button";
 export * from "./components/Chip";
+export * from "./components/Chips";
+export * from "./components/HistoryNav";
 export * from "./components/Input";
 export * from "./components/InputNumber";
 export * from "./components/Switch";
