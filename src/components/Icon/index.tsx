09f9cd8dfedf8e932c6eaaffe3577f6021596357
Vitor Bastos <vbss-services@outlook.com>
2024-06-17 08:42:28 -0300

    feat: Create Icon component

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,17 @@
+import { IconsMap } from "@/components/Icons/icons";
+import { cn } from "@/lib/utils";
+import { ComponentProps, forwardRef } from "react";
+
+export type IconProps = ComponentProps<"div"> & {
+  icon: keyof typeof IconsMap;
+};
+
+export const Icon = forwardRef<HTMLDivElement, IconProps>(
+  ({ className, icon, ...props }, ref) => {
+    return (
+      <div ref={ref} className={cn("flex w-full", className)} {...props}>
+        {IconsMap[icon]}
+      </div>
+    );
+  }
+);
