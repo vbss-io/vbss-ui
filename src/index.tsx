e9c1a9ee9907c7ce4410bf6b7e6ad6ec00a1acd5
Vitor Bastos <vbss-services@outlook.com>
2024-06-20 20:05:26 -0300

    feat: Add new components and update existing ones

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import "./index.css";
 /* eslint-disable react-refresh/only-export-components */
 export * from "./components/Button";
+export * from "./components/Calendar";
+export * from "./components/Checkbox";
 export * from "./components/Chip";
 export * from "./components/Chips";
 export * from "./components/HistoryNav";
+export * from "./components/Icon";
 export * from "./components/Input";
 export * from "./components/InputNumber";
+export * from "./components/RadioGroup";
 export * from "./components/Switch";
+export * from "./components/Textarea";
 export * from "./components/Tooltip";
