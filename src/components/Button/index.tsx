df9ef6018302315fa30beead4faabb4fa5f812c9
Vitor Bastos <vbss-services@outlook.com>
2024-07-18 20:48:54 -0300

    chore: Refactor Button component to use PolymorphicRef and ExtendableButtonProps

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,74 +1,14 @@
 import { IconsMap } from "@/components/Icons";
+import { PolymorphicRef } from "@/lib/types";
 import { cn } from "@/lib/utils";
-import { VariantProps, cva } from "class-variance-authority";
-import { ComponentProps, forwardRef } from "react";
+import { forwardRef } from "react";
+import { buttonStyles } from "./styles";
+import { ButtonComponent, ExtendableButtonProps } from "./types";
 
-export const buttonStyles = cva(
-  "outline-none inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors",
-  {
-    variants: {
-      variant: {
-        primary:
-          "border-2 border-primary bg-primary text-white hover:bg-secondary hover:border-secondary",
-        secondary:
-          "border-2 border-secondary bg-secondary text-white hover:bg-primary hover:border-primary",
-        outline:
-          "border-2 border-primary text-primary bg-background hover:bg-primary/10",
-        "outline-solid":
-          "border-2 border-primary text-primary bg-background hover:bg-primary hover:text-white",
-        ghost: "border-background hover:bg-primary/10 hover:border-primary/10",
-      },
-      size: {
-        sm: "h-8 px-3 text-xs",
-        md: "h-9 px-4 py-2",
-        lg: "h-10 px-8",
-        "icon-sm": "h-8 w-8",
-        "icon-md": "h-9 w-9",
-        "icon-lg": "h-10 w-10",
-      },
-      rounded: {
-        sm: "rounded-sm",
-        md: "rounded-md",
-        lg: "rounded-lg",
-        full: "rounded-full",
-      },
-      font: {
-        regular: "font-normal",
-        medium: "font-medium",
-        bold: "font-bold",
-      },
-      fontSize: {
-        sm: "text-xs",
-        md: "text-sm",
-        lg: "text-md",
-      },
-      inverted: {
-        true: "flex-row-reverse",
-      },
-      disabled: {
-        true: "disabled:pointer-events-none disabled:opacity-50",
-      },
-    },
-    defaultVariants: {
-      variant: "primary",
-      size: "md",
-      rounded: "md",
-      font: "medium",
-      fontSize: "md",
-      inverted: false,
-      disabled: false,
-    },
-  }
-);
-
-export type ButtonProps = ComponentProps<"button"> &
-  VariantProps<typeof buttonStyles> & {
-    icon?: keyof typeof IconsMap;
-  };
-
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (
+export const Button: ButtonComponent = forwardRef(
+  <C extends React.ElementType = "button">(
     {
+      as,
       variant,
       size,
       rounded,
@@ -78,12 +18,14 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       disabled,
       className,
       icon,
+      type = "button",
       ...props
-    },
-    ref
+    }: ExtendableButtonProps<C>,
+    ref?: PolymorphicRef<C>
   ) => {
+    const Component = as ?? "button";
     return (
-      <button
+      <Component
         ref={ref}
         className={cn(
           buttonStyles({
@@ -97,6 +39,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             className,
           })
         )}
+        type={type}
         disabled={disabled}
         {...props}
       >
@@ -108,7 +51,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           </div>
         )}
         {props.children}
-      </button>
+      </Component>
     );
   }
 );
