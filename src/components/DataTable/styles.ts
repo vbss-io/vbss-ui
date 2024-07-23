3b4eefd18755b32288d30b348c3181c17df72bfa
Vitor Bastos <vbss-services@outlook.com>
2024-07-23 08:38:00 -0300

    refactor: Separate DataTable component styles and types and small fixes

---
+++ src/components/DataTable/styles.ts
@@ -0,0 +1,72 @@
+import { cva } from "class-variance-authority";
+
+export const variantStyles = cva("text-black", {
+  variants: {
+    variant: {
+      primary: ``,
+      secondary: ``,
+      outline: ``,
+      ghost: ``,
+    },
+    fontSize: {
+      sm: "text-xs",
+      md: "text-sm",
+      lg: "text-md",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    fontSize: "md",
+  },
+});
+
+export const heightStyles = cva("", {
+  variants: {
+    height: {
+      sm: "h-8",
+      md: "h-10",
+      lg: "h-14",
+    },
+  },
+  defaultVariants: {
+    height: "md",
+  },
+});
+
+export const tableStyles = cva("", {
+  variants: {
+    variant: {
+      primary: ``,
+      secondary: ``,
+      outline: `border-primary`,
+      ghost: ``,
+    },
+    rounded: {
+      sm: "rounded-sm",
+      md: "rounded-md",
+      lg: "rounded-lg",
+    },
+    outBorder: {
+      true: "border",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    rounded: "lg",
+    outBorder: false,
+  },
+});
+
+export const headerStyles: { [key: string]: string } = {
+  primary: "bg-primary text-white",
+  secondary: "bg-secondary text-white",
+  outline: "bg-white text-primary",
+};
+
+export const rowStyles: { [key: string]: string } = {
+  primary: "hover:bg-secondary/5 data-[state=selected]:bg-secondary/10",
+  secondary: "hover:bg-primary/5 data-[state=selected]:bg-primary/10",
+  outline:
+    "border-primary hover:bg-secondary/5 data-[state=selected]:bg-primary/10",
+  ghost: "hover:bg-secondary/5 data-[state=selected]:bg-primary/10",
+};
