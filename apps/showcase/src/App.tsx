2a8256cc532084bb267320d2f85832e3a879631d
Vitor Bastos <vbss-services@outlook.com>
2025-03-03 18:59:47 -0300

    feat(showcase): simplify showcase app

--- apps/showcase/src/App.tsx
+++ apps/showcase/src/App.tsx
@@ -1,11 +1,9 @@
-import { Chip } from "vbss-ui-chip";
+import { Chip } from "@vbss-ui/chip";
 
 function App() {
   return (
-    <div style={{ display: 'flex', padding: '1rem', gap: '1rem' }}>
-      <Chip as={'div'} variant={'primary'} size={'sm'}>Oi</Chip>
-      <Chip variant={'secondary'} size={'md'}>Oi</Chip>
-      <Chip variant={'outline'} size={'lg'}>Oi</Chip>
+    <div>
+      <Chip>Oi</Chip>
     </div>
   );
 }
