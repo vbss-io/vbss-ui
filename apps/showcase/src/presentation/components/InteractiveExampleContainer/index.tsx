d16b0d1bb0fede6725e3f98165a32390de7fac4a
Vitor Bastos <vbss-services@outlook.com>
2025-04-27 11:39:08 -0300

    feat(docs): interactive example container component

---
+++ apps/showcase/src/presentation/components/InteractiveExampleContainer/index.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@vbss-ui/button"
+import { cloneElement, isValidElement, ReactNode, useState } from "react"
+
+import { CodeSnippet } from "@/presentation/components/CodeSnippet"
+
+import * as S from "./styles"
+
+interface InteractiveExampleContainerProps<T> {
+  props: T[]
+  targetProp: string
+  children: ReactNode
+  code?: string
+}
+
+export const InteractiveExampleContainer = <T extends string>({
+  children,
+  code,
+  props,
+  targetProp,
+}: InteractiveExampleContainerProps<T>) => {
+  const [selectedProp, setSelectedProp] = useState<T>(props[0])
+  const updatedCode = code?.replace(`'${targetProp}'`, `'${selectedProp}'`)
+  const enhancedChildren = isValidElement(children)
+    ? cloneElement(children, { [targetProp]: selectedProp })
+    : children
+
+  return (
+    <S.Container>
+      <S.Controls>
+        {props.map((prop) => (
+          <Button
+            key={prop}
+            variant={selectedProp === prop ? "primary" : "secondary"}
+            onClick={() => setSelectedProp(prop)}
+          >
+            {prop}
+          </Button>
+        ))}
+      </S.Controls>
+      {enhancedChildren}
+      {updatedCode && (
+        <S.Content>
+          <CodeSnippet code={updatedCode} language="tsx" />
+        </S.Content>
+      )}
+    </S.Container>
+  )
+}
---
+++ apps/showcase/src/presentation/components/InteractiveExampleContainer/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+
+export const Controls = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem;
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
