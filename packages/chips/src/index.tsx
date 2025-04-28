8041f6630664fb3cac85688e6cba43d5e79d7963
Vitor Bastos <vbss-services@outlook.com>
2025-04-28 08:43:31 -0300

    chore(chips): apply lint rules

--- packages/chips/src/index.tsx
+++ packages/chips/src/index.tsx
@@ -1,50 +1,36 @@
-import { Chip, ExtendableChipProps } from "@vbss-ui/chip";
-import { ExtendableComponentProps, PolymorphicRef, cn } from "@vbss-ui/lib";
-import { ComponentProps, ElementType, forwardRef, ForwardRefExoticComponent, ReactNode, RefAttributes } from "react";
-import { cva, VariantProps } from "class-variance-authority";
+import { Chip, ExtendableChipProps } from "@vbss-ui/chip"
+import { cn, ExtendableComponentProps, PolymorphicRef } from "@vbss-ui/lib"
+import { cva, VariantProps } from "class-variance-authority"
+import { ComponentProps, ElementType, forwardRef, ForwardRefExoticComponent, ReactNode, RefAttributes } from "react"
 
-type ChipsProps = ComponentProps<"div"> & VariantProps<typeof chipsStyles> & {
-  chips?: string[] | number[] | ReactNode[];
-  chipsProps?: ExtendableChipProps<ElementType>;
-}
+type ChipsProps = ComponentProps<"div"> &
+  VariantProps<typeof chipsStyles> & {
+    chips?: string[] | number[] | ReactNode[]
+    chipsProps?: ExtendableChipProps<ElementType>
+  }
 
-export type ExtendableChipsProps<C extends ElementType> =
-  ExtendableComponentProps<C, ChipsProps>;
+export type ExtendableChipsProps<C extends ElementType> = ExtendableComponentProps<C, ChipsProps>
 
-export type ChipsComponent = ForwardRefExoticComponent<
-  ExtendableChipsProps<ElementType> & RefAttributes<ElementType>
->;
+export type ChipsComponent = ForwardRefExoticComponent<ExtendableChipsProps<ElementType> & RefAttributes<ElementType>>
 
 /**
  * The Chips component is a wrapper for grouping multiple Chip components.
  */
 export const Chips: ChipsComponent = forwardRef(
   <C extends ElementType>(
-    {
-      as,
-      gap,
-      flexDirection,
-      className,
-      chips,
-      chipsProps,
-      ...props
-    }: ExtendableChipsProps<C>,
+    { as, gap, flexDirection, className, chips, chipsProps, ...props }: ExtendableChipsProps<C>,
     ref?: PolymorphicRef<C>
   ) => {
-    const Component = as ?? "div";
+    const Component = as ?? "div"
     return (
-      <Component
-        className={cn("chips", chipsStyles({ gap, flexDirection, className }))}
-        ref={ref}
-        {...props}
-      >
+      <Component className={cn("chips", chipsStyles({ gap, flexDirection, className }))} ref={ref} {...props}>
         {chips?.map((chip, index) => (
           <Chip key={index} {...chipsProps}>
             {chip}
           </Chip>
         ))}
       </Component>
-    );
+    )
   }
 )
 
@@ -57,15 +43,15 @@ export const chipsStyles = cva("leading-none inline-flex items-center", {
       sm: "gap-1",
       md: "gap-2",
       lg: "gap-3",
-      xl: "gap-4"
+      xl: "gap-4",
     },
     flexDirection: {
       row: "flex-row",
-      col: "flex-col"
-    }
+      col: "flex-col",
+    },
   },
   defaultVariants: {
     gap: "md",
-    flexDirection: "row"
+    flexDirection: "row",
   },
-});
+})
