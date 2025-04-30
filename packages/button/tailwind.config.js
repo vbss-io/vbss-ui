97e610c9d65332e88b690e97086bd7d041d392a3
Vitor Bastos <vbss-services@outlook.com>
2025-04-30 07:47:06 -0300

    feat(button): minor changes

--- packages/button/tailwind.config.js
+++ packages/button/tailwind.config.js
@@ -1,8 +1,8 @@
-import baseConfig from "../../tailwind.config";
+import baseConfig from "../../tailwind.config"
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["../**/src/**/*.{js,ts,jsx,tsx}"],
   theme: baseConfig.theme,
   plugins: [],
-};
+}
