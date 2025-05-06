56be94a0ad977a51b350591a3fc54e88e8ffa7df
Vitor Bastos <vbss-services@outlook.com>
2025-05-06 08:27:14 -0300

    feat(storybook): configure storybook to load svg icons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,29 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/react-vite"
+import svgr from "vite-plugin-svgr"
 
 const config: StorybookConfig = {
-  stories: [
-    "../packages/@stories/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  addons: [
-    "@storybook/addon-essentials",
-  ],
+  stories: ["../packages/@stories/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-};
-export default config;
+  async viteFinal(config) {
+    return {
+      ...config,
+      plugins: [
+        ...(config.plugins || []),
+        svgr({
+          svgrOptions: {
+            exportType: "default",
+            ref: true,
+            svgo: false,
+            titleProp: true,
+          },
+          include: "**/*.svg",
+        }),
+      ],
+    }
+  },
+}
+export default config
